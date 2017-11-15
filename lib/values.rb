89dd1ece030da53c7495c38ebf24c899236e048a
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-15 21:09:22 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,7 +9,8 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  #code your solution here
+  valueList = []
     groceries.each do |cat, food|
       valueList << food
     end
